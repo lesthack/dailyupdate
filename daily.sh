779482816c57e6e44fc244d6c2d28bca5a229ee0
Jorge Hernández <lesthack@gmail.com>
2016-05-13 14:02:15 -0400

    Changes on 2016-05-13 14:02

--- daily.sh
+++ daily.sh
@@ -22,6 +22,20 @@ fi
 python lastfm.py -p "$path" -s
 
 #echo "." >> README.md
+AGENT="ssh-agent -s"
+if [ ! -d $HOME/.ssh/agent ]; then
+    mkdir -p $HOME/.ssh/agent
+fi
+#
+# Start an agent if there isn't one running already.
+#
+pid=`ps -u$LOGNAME | grep ssh-age | awk '{print $1}'`
+if [ -z "$pid" ]; then
+    $AGENT | grep -v echo > $HOME/.ssh/agent/$HOST & pid=$!
+    sleep 1 # Let it fork and stuff
+fi
+ssh-add $HOME/.ssh/git_rsa
+
 git add *
 git commit -m "Changes on $date"
 git push origin master
